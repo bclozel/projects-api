5e27f9794dbb35bf74484dad57fdfa4eba44bf41
Brian Clozel <bclozel@vmware.com>
2021-09-30 15:58:50 +0200

    Add Mutation query for creating a Project Release
    This commit adds a new Mutation query that creates a new Release and
    adds it to the given Project.

--- src/main/java/io/spring/sample/projects/ProjectsController.java
+++ src/main/java/io/spring/sample/projects/ProjectsController.java
@@ -4,8 +4,11 @@ import java.util.Optional;
 
 import io.spring.sample.projects.metadata.Project;
 import io.spring.sample.projects.metadata.Projects;
+import io.spring.sample.projects.metadata.Release;
+import io.spring.sample.projects.metadata.Version;
 
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -22,4 +25,14 @@ public class ProjectsController {
 	public Optional<Project> project(@Argument String slug) {
 		return this.projects.findBySlug(slug);
 	}
+
+	@MutationMapping
+	public Optional<Release> createRelease(@Argument String projectSlug, @Argument Version version) {
+		return this.projects.findBySlug(projectSlug).map(p -> {
+			Release release = p.createRelease(version);
+			this.projects.save(p);
+			return release;
+		});
+	}
+
 }
